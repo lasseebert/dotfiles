146b8c1a0142ead6afccebcf1bba7e51c86dc529
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-10-10 14:44:14 +0200

    Show entire path in prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,14 +2,18 @@
 # This is set to empty to not greet at all
 set fish_greeting
 
+# Show full path in prompt
+# This variable tells the prompt how many chars is visible per path part
+set fish_prompt_pwd_dir_length 100
+
 # Editor used in various programs
 set -x EDITOR vim
 
-# Set up asdf
-source ~/.asdf/asdf.fish
-
 # Enable iex history
 set -x ERL_AFLAGS $ERL_AFLAGS "-kernel shell_history enabled"
+
+# Set up asdf
+source ~/.asdf/asdf.fish
 
 # Wow. Such doge. Very zsh. Many terminal. Wow
 # https://github.com/thiderman/doge
