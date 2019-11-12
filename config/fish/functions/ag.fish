4e4e87261c0412a35c0a95fcc311d2f85d2fa4a1
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2019-11-12 09:49:21 +0100

    Search ag case sensitive

--- config/fish/functions/ag.fish
+++ config/fish/functions/ag.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 0
+# Defined in /tmp/fish.vmx2qT/ag.fish @ line 2
 function ag --description 'alias ag ag --skip-vcs-ignores'
-	command ag --skip-vcs-ignores $argv;
+	command ag --skip-vcs-ignores --case-sensitive $argv;
 end
