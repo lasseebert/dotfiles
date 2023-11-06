37c990a57404202b5aef7084e1872dfac0602456
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2023-11-06 08:33:52 +0100

    Add ruby to treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = {"elixir", "heex", "eex", "lua"},
+      ensure_installed = {"elixir", "heex", "eex", "lua", "ruby"},
       sync_install = false,
       ignore_install = { },
       highlight = {
