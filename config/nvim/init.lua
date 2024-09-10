a57ca14d108461a92f8972a45907be58897526c2
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2024-09-10 12:21:41 +0200

    nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -20,8 +20,9 @@ vim.keymap.set("n", "Y", "Y")
 -- Hide incsearch highlight
 vim.keymap.set("n", "//", ":nohlsearch<CR>")
 
--- Show line numbers
-vim.wo.number = true
+-- Line numbers
+vim.wo.number = true -- Show line numbers
+vim.o.relativenumber = true -- Set relative numbered lines
 
 -- Show invisible chars
 vim.wo.listchars = "tab:▷⋅,trail:·"
@@ -50,3 +51,117 @@ vim.opt.scrolloff = 5
 vim.opt.foldmethod = "indent" -- Fold based on indent level.
 vim.opt.foldenable = false -- Disable folding at startup.
 vim.opt.foldlevelstart = 99 -- Open all folds by default.
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
+
+-- Vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+
+
+-- All of this is copied from https://github.com/hendrikmi/neovim-kickstart-config/blob/main/lua/core/options.lua
+--vim.o.wrap = false -- Display lines as one long line (default: true)
+--vim.o.linebreak = true -- Companion to wrap, don't split words (default: false)
+--vim.o.mouse = 'a' -- Enable mouse mode (default: '')
+--vim.o.autoindent = true -- Copy indent from current line when starting new one (default: true)
+--vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+--vim.o.smartcase = true -- Smart case (default: false)
+--vim.o.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
+--vim.o.tabstop = 4 -- Insert n spaces for a tab (default: 8)
+--vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+--vim.o.expandtab = true -- Convert tabs to spaces (default: false)
+--vim.o.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
+--vim.o.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
+--vim.o.cursorline = false -- Highlight the current line (default: false)
+--vim.o.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
+--vim.o.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
+--vim.o.hlsearch = false -- Set highlight on search (default: true)
+--vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
+--vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+--vim.o.whichwrap = 'bs<>[]hl' -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
+--vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+--vim.o.swapfile = false -- Creates a swapfile (default: true)
+--vim.o.smartindent = true -- Make indenting smarter again (default: false)
+--vim.o.showtabline = 2 -- Always show tabs (default: 1)
+--vim.o.backspace = 'indent,eol,start' -- Allow backspace on (default: 'indent,eol,start')
+--vim.o.pumheight = 10 -- Pop up menu height (default: 0)
+--vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+--vim.wo.signcolumn = 'yes' -- Keep signcolumn on by default (default: 'auto')
+--vim.o.fileencoding = 'utf-8' -- The encoding written to a file (default: 'utf-8')
+--vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
+--vim.o.breakindent = true -- Enable break indent (default: false)
+--vim.o.updatetime = 250 -- Decrease update time (default: 4000)
+--vim.o.timeoutlen = 300 -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
+--vim.o.backup = false -- Creates a backup file (default: false)
+--vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+--vim.o.undofile = true -- Save undo history (default: false)
+--vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience (default: 'menu,preview')
+--vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+--vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
+--vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+--vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+
+
+
+
+
+
+-- All of this is copied from https://github.com/hendrikmi/neovim-kickstart-config/blob/main/lua/core/keymaps.lua
+--
+---- Disable the spacebar key's default behavior in Normal and Visual modes
+--vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+--
+---- For conciseness
+--local opts = { noremap = true, silent = true }
+--
+---- Resize with arrows
+--vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+--vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+--vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+--vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+--
+---- Buffers
+--vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+--vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+--vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts) -- close buffer
+--vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+--
+---- Window management
+--vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+--vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+--vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+--vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+--
+---- Navigate between splits
+--vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+--vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+--vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+--vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+--
+---- Tabs
+--vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+--vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+--vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+--vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+--
+---- Toggle line wrapping
+--vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+--
+---- Diagnostic keymaps
+--vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+--vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+--vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+--vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
