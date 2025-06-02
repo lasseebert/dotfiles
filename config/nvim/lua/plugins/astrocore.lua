701d7500328e35f98f02ac4f9c0d740511177fa8
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-06-02 13:05:59 +0200

    Configure rbi files

--- config/nvim/lua/plugins/astrocore.lua
+++ config/nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -28,7 +26,8 @@ return {
     filetypes = {
       -- see `:h vim.filetype.add` for usage
       extension = {
-        foo = "fooscript",
+        -- Sorbet type files for Ruby
+        rbi = 'ruby',
       },
       filename = {
         [".foorc"] = "fooscript",
--- todo.md
+++ todo.md
@@ -8,3 +8,11 @@
 * Elixir LSP
 * Ruby LSP
 * Greplace or similar
+
+## Astronvim
+
+* Should not search case-insensitive
+* Enable or remove all config files
+* 5 lines around cursor
+* Copilot
+* Make goto-definition and other things work in Ruby
