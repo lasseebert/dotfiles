ff9fac3771464ba8b8b1d45934ed9386109723eb
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-06-04 14:54:24 +0200

    Fix indentation in Ruby
    And add a bunch of other languages to treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,38 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "lua",
-      "vim",
-      "ruby",
+      'bash',
+      'cmake',
+      'css',
+      'dockerfile',
+      'elixir',
+      'gitignore',
+      'graphql',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'make',
+      'markdown',
+      'markdown_inline',
+      'regex',
+      'ruby',
+      'sql',
+      'terraform',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'yaml',
     },
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
   },
 }
