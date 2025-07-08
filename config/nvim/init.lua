39c23284309bb27472a6814adb98ee5005673188
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-08 23:58:49 +0200

    Set nerd fonts

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -119,7 +119,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
