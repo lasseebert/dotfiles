08bd8b5d232ab6efd48727493ce217f839a3ed4b
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-09 21:34:37 +0200

    Remove unused plugins

--- config/nvim/lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- https://github.com/mason-org/mason.nvim
-  'mason-org/mason.nvim',
-  -- See default settings here:
-  -- https://github.com/mason-org/mason.nvim?tab=readme-ov-file#default-configuration
-  opts = {},
-}
