0a40e3e6d43e85bba7fb3bcf1a407025d52f601d
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-09 15:38:25 +0200

    Blink-cmp: Add default preset as comment

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -743,6 +743,21 @@ require('lazy').setup({
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+
+
+        -- This is the default preset:
+        -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        -- ['<C-e>'] = { 'hide' },
+        -- ['<C-y>'] = { 'select_and_accept' },
+        -- ['<Up>'] = { 'select_prev', 'fallback' },
+        -- ['<Down>'] = { 'select_next', 'fallback' },
+        -- ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        -- ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+        -- ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        -- ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        -- ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
 
       appearance = {
