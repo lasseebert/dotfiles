66c2f2cb5840c172c66a813ff13981265ba6bd03
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-09 21:31:17 +0200

    Don't show user friendly box, eww

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,11 +29,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
