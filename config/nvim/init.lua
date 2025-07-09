a97f291b7301a1b546930943a03507f8cb4b1f99
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-09 21:30:22 +0200

    Don't show split window when replacing

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,9 +26,6 @@ vim.o.timeoutlen = 300
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
