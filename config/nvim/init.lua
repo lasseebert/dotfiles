5bf95326d35f47e041ff91bf1679789ec2713c4e
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2025-07-13 19:38:30 +0200

    Elixirls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -344,6 +344,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        elixirls = {},
 
         lua_ls = {
           -- cmd = { ... },
