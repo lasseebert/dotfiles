145097e480560a27d99d5d62c7edf2a3b5a194f4
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2026-01-13 22:25:23 +0100

    Move todo-comments to plugin file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -111,8 +111,7 @@ require('lazy').setup({
     },
   },
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  require 'plugins.todo-comments',
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
