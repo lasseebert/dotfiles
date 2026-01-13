e8801b5774f8a752d413183545a0efd843da1736
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2026-01-13 22:08:29 +0100

    Remove autoformat

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,33 +22,6 @@ require('lazy').setup({
   require 'plugins.telescope',
   require 'plugins.gitsigns',
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
-
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
