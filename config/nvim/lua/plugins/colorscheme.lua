cf3017a452eab95064fba0ec3e7c731b4a10b908
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2026-08-15 13:53:03 +0200

    Do not italix Elixir module names

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,3 +3,9 @@ vim.pack.add({'https://github.com/catppuccin/nvim.git'})
 require('catppuccin').setup({})
 
 vim.cmd.colorscheme("catppuccin")
+
+-- Disable italics for Elixir module names
+local elixir_module_highlight = vim.api.nvim_get_hl(0, { name = '@module.elixir', link = false })
+elixir_module_highlight.italic = false
+elixir_module_highlight.cterm = vim.tbl_extend('force', elixir_module_highlight.cterm or {}, { italic = false })
+vim.api.nvim_set_hl(0, '@module.elixir', elixir_module_highlight)
