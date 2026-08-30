0a88b0a17cd9c427e23ebc625d9821b1c8d461cc
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2026-08-30 21:32:01 +0200

    Change oil shortcuts to not use Ctrl+[hjkl]

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -76,11 +76,9 @@ require("oil").setup({
   keymaps = {
     ["g?"] = { "actions.show_help", mode = "n" },
     ["<CR>"] = "actions.select",
-    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
     ["<C-t>"] = { "actions.select", opts = { tab = true } },
     ["<C-c>"] = { "actions.close", mode = "n" },
-    ["<C-l>"] = "actions.refresh",
+    ["<C-r>"] = "actions.refresh",
     ["-"] = { "actions.parent", mode = "n" },
     ["_"] = { "actions.open_cwd", mode = "n" },
     ["`"] = { "actions.cd", mode = "n" },
